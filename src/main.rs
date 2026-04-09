a3d6754aeab2fa34f5d2d8b77051ab02720a72d6
Martin Liska <martin.liska@hey.com>
2026-04-09 16:51:19 +0200

    include also wasmedge

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ fn main() {
         .expect("wasmtime module should call the host function successfully");
     let wasmer_calls = host_fn_benchmark::call_hello_1000_times_wasmer()
         .expect("wasmer module should call the host function successfully");
+    let wasmedge_calls = host_fn_benchmark::call_hello_1000_times_wasmedge()
+        .expect("wasmedge module should call the host function successfully");
     println!("Executed {wasmi_calls} host calls with wasmi");
     println!("Executed {wasmtime_calls} host calls with wasmtime");
     println!("Executed {wasmer_calls} host calls with wasmer");
+    println!("Executed {wasmedge_calls} host calls with wasmedge");
 }
